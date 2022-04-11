ee70f0dd0419d753b3209e4a0fab96a030ecbe2f
ciaranvinken <81919402+ciaranvinken@users.noreply.github.com>
2022-04-11 15:09:33 +0200

    ciaran

--- ciaran/src/be/craftcode/scrabble/domain/Letter.java
+++ ciaran/src/be/craftcode/scrabble/domain/Letter.java
@@ -6,8 +6,8 @@ public enum Letter {
     O('o', 1), P('p', 3), Q('q', 10), R('r', 1), S('s', 1), T('t', 1), U('u', 1),
     V('v', 4), W('w', 4), X('x', 8), Y('y', 4), Z('z', 10);
 
-    private char letter;
-    private int value;
+    private final char letter;
+    private final int value;
 
     private Letter(char letter, int value) {
         this.letter = letter;
